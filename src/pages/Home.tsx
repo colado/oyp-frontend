42206346c1769be6b6220fd21bb7351e9ee4777d
colado <33748103+colado@users.noreply.github.com>
2024-10-17 18:48:27 +0300

    Reintegrate check for access token

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,79 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { isAuthenticated } from "../utils/auth";
 
 export default function Home() {
+  // const [playlists, setPlaylists] = useState(null);
   const [imported, setImported] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const authenticated = await isAuthenticated();
+      if (!authenticated) {
+        navigate("/login");
+      } else {
+        setLoading(false);
+      }
+    };
+    checkAuth();
+  }, [navigate]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  useEffect(() => {
+    const fetchPlaylistsFromIPFS = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:3000/playlists/get-playlists/your-cid-here"
+        );
+        const data = await response.json();
+
+        if (response.ok) {
+          setImported(true); // Save the playlist data from IPFS
+        } else {
+          throw new Error(data.message || "Failed to fetch playlists");
+        }
+      } catch (error) {
+        console.log("Error importing playlists from IPFS");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPlaylistsFromIPFS();
+  }, []);
 
   const importAndSavePlaylists = async () => {
     setLoading(true);
 
     try {
       // Step 1: Fetch playlists from Spotify
-      const importResponse = await fetch("http://localhost:3000/spotify/import", {
-        method: "POST",
-        credentials: "include", // Include cookies in the request
-      });
+      const importResponse = await fetch(
+        "http://localhost:3000/spotify/import",
+        {
+          method: "POST",
+          credentials: "include", // Include cookies in the request
+        }
+      );
 
       if (importResponse.ok) {
         const { playlists } = await importResponse.json();
 
         // Step 2: Save playlists to IPFS
-        const saveResponse = await fetch("http://localhost:3000/playlists/save", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ playlists }),
-        });
+        const saveResponse = await fetch(
+          "http://localhost:3000/playlists/save",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ playlists }),
+          }
+        );
 
         if (saveResponse.ok) {
           const data = await saveResponse.json();
